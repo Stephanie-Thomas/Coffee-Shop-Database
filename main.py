02813a2327541a738c9a3856d8782c5c1cad551f
Stephanie-Thomas <107421799+Stephanie-Thomas@users.noreply.github.com>
2024-05-31 06:49:19 -0700

    added another feature to main table, wrote code for add function

--- main.py
+++ main.py
@@ -1,136 +1,96 @@
 import mysql.connector
-import customer
-import employee
+
+     
+host = input("Enter the host name you want to connect to: ")
+user_name = input("Enter the user name: ")
+passwrd = input("Enter the password: ")
+db_name = input("Enter the database name: ")
 
 
+db = mysql.connector.connect(
+            host=host,
+            user=user_name,
+            password=passwrd,
+            database=db_name
+        )
+mycursor = db.cursor()
 
 def display_menu():
     while True:
         print("\nMain Menu:")
-        print("1. Employee Management")
-        print("2. Customer Management")
-        print("3. Inventory Management")
-        print("4. Order Management")
-        print("5. Exit")
-
-        user_option = int(input("Enter your choice (1-5): "))
-        
-        if user_option == 1:
-            employee_management_menu()
-        elif user_option == 2:
-            customer_management_menu()
-        elif user_option == 3:
-            inventory_management_menu()
-        elif user_option == 4:
-            order_management_menu()
-        elif user_option == 5:
-            print("Exiting the program.")
-            break
-        else:
-            print("Invalid choice. Please enter a number from 1 to 5.")
-
-
-def employee_management_menu():
-     while True:
-        print("\nEmployee Management:")
-        print("1. Add new employee")
-        print("2. Remove employee")
-        print("3. Update employee details")
-        print("4. View employees")
-        print("5. Search employees")
-        print("6. Back to Main Menu")
+        print("1. Add")
+        print("2. Remove")
+        print("3. Update")
+        print("4. View")
+        print("5. Search")
+        print("6. Quit")
 
         user_option = int(input("Enter your choice (1-6): "))
-       
-
+        
         if user_option == 1:
-            add_new_employee()
+            Add()
         elif user_option == 2:
-            remove_employee()
+            Remove()
         elif user_option == 3:
-            update_employee_details()
+            Update()
         elif user_option == 4:
-            view_employees()
+           View()
         elif user_option == 5:
-            search_employees()
+            Search()
         elif user_option == 6:
+            print("Exiting the program")
             break
         else:
-            print("Invalid choice. Please enter a number from 1 to 6")
+            print("Invalid choice. Please enter a number from 1 to 6.")
 
-def customer_management_menu():
+
+def Add():
     while True:
-        print("\nCustomer Management:")
-        print("1. Add new customer")
-        print("2. Remove customer")
-        print("3. Update customer details")
-        print("4. View customers")
-        print("5. Search customers")
-        print("6. Back to Main Menu")
+        print("What table would you like to add to:")
+        print("\n1.Add A Customer")
+        print("2.Add An Employee")
+        print("3.Add something to Inventory")
+        print("4.Add an item to orders")
 
-        user_option = int(input("Enter your choice (1-6): "))
-        
-        
+        user_option = int(input("Enter your choice (1-4): "))
         if user_option == 1:
-            add_new_customer()
+            customer_no = int(input("Enter the customer number: "))
+            first_name =  input("Enter the first name: ")
+            last_name = input("Enter the last name: ")
+            phone_num = int(input("Enter the phone number: "))
+            email = input("Enter the email: ")
+
+            mycursor.execute("INSERT INTO customer_info (customer_No, first_name, last_name, phone_num, email) VALUES (%s,%s, %s, %s, %s)", (customer_no, first_name, last_name, phone_num, email))
+            db.commit
+
+            print("\nHere's the new dataset: ")
+
+            mycursor.execute("Select *  FROM customer_info")
+
+            for x in mycursor:
+                print(x)
+            break
         elif user_option == 2:
-            remove_customer()
-        elif user_option == 3:
-            update_customer_details()
-        elif user_option == 4:
-            view_customers()
-        elif user_option == 5:
-            search_customers()
-        elif user_option == 6:
+            employee_id = int(input("Enter an employee id: "))
+            E_first_name = input("Enter the employee's first name: ")
+            E_last_name = input("Enter the last name: ")
+            hourly_pay = int(input("Enter the hourly pay: "))
+            title = input("What their title: ")
+            
+            mycursor.execute("INSERT INTO employee_info (employee_id, first_name, last_name, hourly_pay, title) VALUES (%s,%s, %s, %s, %s)", (employee_id, E_first_name, E_last_name, hourly_pay, title))
+            db.commit
+
+            print("\nHere's the new dataset: ")
+
+            mycursor.execute("Select *  FROM customer_info")
+
+            for x in mycursor:
+                print(x)
             break
-        else:
-            print("Invalid choice. Please enter a number from 1 to 6")
-
-
-def inventory_management_menu():
-    while True:
-        print("\nInventory Management:")
-        print("1. Add new item")
-        print("2. Remove item")
-        print("3. Update item details")
-        print("4. View inventory")
-        print("5. Search inventory")
-        print("6. Back to Main Menu")
-
-        user_option = int(input("Enter your choice (1-6): "))
         
-        if user_option == 6:
-            break
-def order_management_menu():
-    while True:
-        print("\nOrder Management:")
-        print("1. Place new order")
-        print("2. Cancel order")
-        print("3. Update order")
-        print("4. View orders")
-        print("5. Search orders")
-        print("6. Back to Main Menu")
-
-        user_option = int(input("Enter your choice (1-6): "))
-        # Implement actions for order management based on user choice
-        if user_option == 6:
-            break
 
 
-print("Welcome! This program is an interactive database management application\n")
 
-
-host = input("Enter the host name you want to connect to: ")
-user_name = input("Enter the user name: ")
-passwrd = input("Enter the passowrd: ")
-db_name = input("Enter the database: ")
-
-db = mysql.connector.connect(
-    host = host,
-    user = user_name,
-    password = passwrd,
-    database = db_name
-)
 
 
 display_menu()
